a18a928756ec4f7324059af55173019fbf82d1e5
Shuai Zhang <zhangshuai.ustc@gmail.com>
2017-09-30 23:50:06 +0800

    Use master branch script.

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@
 if [ "$(uname)" == "Darwin" ]; then
     export DOTNET_INSTALL_DIR="$PWD/.dotnetsdk"
     export CLI_VERSION="2.0.0"
-    curl -sSL https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh | bash /dev/stdin --version "$CLI_VERSION" --install-dir "$DOTNET_INSTALL_DIR"
+    curl -sSL https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.sh | bash /dev/stdin --version "$CLI_VERSION" --install-dir "$DOTNET_INSTALL_DIR"
     export PATH="$DOTNET_INSTALL_DIR:$PATH"
+    #https://download.microsoft.com/download/1/B/4/1B4DE605-8378-47A5-B01B-2C79D6C55519/dotnet-sdk-2.0.0-osx-x64.tar.gz
 else
     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
     sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
