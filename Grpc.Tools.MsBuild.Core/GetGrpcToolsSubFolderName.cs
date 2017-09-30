7861bc1888e606930ff7033c00cf8fb4b4cbb70d
Shuai Zhang <zhangshuai.ustc@gmail.com>
2017-09-30 10:24:30 +0800

    Initial commit.

---
+++ Grpc.Tools.MsBuild.Core/GetGrpcToolsSubFolderName.cs
@@ -0,0 +1,53 @@
+﻿#region Copyright notice and license
+
+// Copyright 2017 Shuai Zhang.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using Grpc.Core.Internal;
+using Microsoft.Build.Framework;
+
+namespace Grpc.Tools.MsBuild.Core
+{
+    public class GetGrpcToolsSubFolderName : Microsoft.Build.Utilities.Task
+    {
+        public override bool Execute()
+        {
+            try
+            {
+                GrpcToolsSubFolderName = GetOsPrefix() + "_" + (PlatformApis.Is64Bit ? "x64" : "x86");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.LogErrorFromException(ex);
+                return false;
+            }
+        }
+
+        private string GetOsPrefix()
+        {
+            if (PlatformApis.IsWindows) return "windows";
+            if (PlatformApis.IsLinux) return "linux";
+            if (PlatformApis.IsMacOSX) return "macosx";
+
+            throw new InvalidOperationException("Cannot determine your system.");
+        }
+
+        [Output]
+        public string GrpcToolsSubFolderName { get; private set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,58 @@
+# Grpc.Tools.MsBuild #
+
+This package helps MSBuild run Grpc tools generate CSharp codes from proto files &
+integrate them into building automatically.
+
+## Getting Started ##
+
+### Install ###
+
+```
+Install-Package Grpc.Tools.MsBuild.Unofficial
+```
+
+### Auto Discover ###
+
+The package would automatically discover *.proto files under your project directory.
+
+If you need add more files out of your project directory, you are able to include them by add such section in your poject file:
+
+```xml
+<ItemGroup>
+  <ProtoFile Include="..\protos\helloworld.proto" />
+</ItemGroup>
+```
+
+You might also need to add more folders for Grpc includes:
+
+```xml
+<PropertyGroup>
+  <GrpcIncludeFolders>..\protos\;.</GrpcIncludeFolders>
+</PropertyGroup>
+```
+
+### Overrides ###
+
+You are able to override default behaviors by assign Property in your project file.
+
+* GrpcToolsVersion: 1.6.1
+* GrpcToolsPath: $(UserProfile)\.nuget\packages\grpc.tools\$(GrpcToolsVersion)\tools\
+* GrpcCSharpPluginExecName: grpc_csharp_plugin
+* GrpcProtocExecName: protoc
+* GrpcIncludeFolders: .
+* AutoDiscoverProjectProtocFiles: true
+* GrpcCSharpPluginExec: null
+* GrpcProtocExec: null
+* GrpcAdditionalArguments: null
+
+## Todo ##
+
+1. Run manual test for baseline
+1. Run manual test for intellisense
+1. Prepare test project
+1. Add CI for testing on Windows & Linux
+1. Merge into Grpc repository
+
+## License ##
+
+This project is licensed under Apache 2.0 license -- See [LICENSE.txt](LICENSE.txt) file for details.
