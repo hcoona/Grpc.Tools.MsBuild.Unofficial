74da45af1fb1aafadfc485db765aa249283c0bdb
Shuai Zhang <zhangshuai.ustc@gmail.com>
2017-09-30 13:53:23 +0800

    Add Test project.

---
+++ Tests/GreetingClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GreetingClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ build.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = "Stop"
+
+$NuGetPackageRoot = (nuget locals global-packages -list | ConvertFrom-String -PropertyNames Key, Value).Value
+$SelfPackageCache = Join-Path $NuGetPackageRoot Grpc.Tools.MsBuild.Unofficial
+if (Test-Path $SelfPackageCache) {
+    Remove-Item $SelfPackageCache -Recurse -Force -ErrorAction "Continue"
+}
+
+Get-ChildItem (Join-Path $PSScriptRoot Grpc.Tools.MsBuild.Core\bin) -Recurse -Filter *.nupkg -File | Remove-Item -Force
+
+dotnet restore Grpc.Tools.MsBuild.Core\Grpc.Tools.MsBuild.Core.csproj
+dotnet build Grpc.Tools.MsBuild.Core\Grpc.Tools.MsBuild.Core.csproj
+
+dotnet restore Tests\GreetingClient\GreetingClient.csproj
+dotnet build Tests\GreetingClient\GreetingClient.csproj -v normal
